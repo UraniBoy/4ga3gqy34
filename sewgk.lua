7d95c3a1556f5482b9a4197f56e083253ee9052c
UraniBoy <ardilkasacitr@gmail.com>
2024-09-23 00:07:26 +0200

    sewgk.lua

---
+++ sewgk.lua
@@ -0,0 +1,3 @@
+getgenv().TestBuild = false
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CeleryHub/CeleryHub/refs/heads/main/SecondCommunityWarningThanksNick.lua"))()
+warn(" MADE BY FORTNITE_DEMON")
